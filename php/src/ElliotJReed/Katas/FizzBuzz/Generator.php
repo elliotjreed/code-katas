ddeb0f71bd7dad0b1fc5c4ff9c63d67d35ff4c29
Elliot J. Reed <elliot.reed@bunches.co.uk>
2018-07-16 22:26:06 +0100

    Adds FizzBuzz in PHP

--- php/src/ElliotJReed/Katas/FizzBuzz/Generator.php
+++ php/src/ElliotJReed/Katas/FizzBuzz/Generator.php
@@ -5,8 +5,41 @@ namespace ElliotJReed\Katas\FizzBuzz;
 
 class Generator
 {
-    public function schedule(): string
+    public function schedule(): array
     {
-        return '1';
+        $list = [];
+        for ($playerTurn = 1; $playerTurn <= 100; ++$playerTurn) {
+            if ($this->isFizz($playerTurn) || $this->isBuzz($playerTurn)) {
+                $list[] = $this->buildFizzBuzzWord($playerTurn);
+            } else {
+                $list[] = (string) $playerTurn;
+            }
+        }
+
+        return $list;
+    }
+
+    private function buildFizzBuzzWord(int $playerTurn): string
+    {
+        $fizzBuzzWord = '';
+        if ($this->isFizz($playerTurn)) {
+            $fizzBuzzWord .= 'Fizz';
+        }
+
+        if ($this->isBuzz($playerTurn)) {
+            $fizzBuzzWord .= 'Buzz';
+        }
+
+        return $fizzBuzzWord;
+    }
+
+    private function isFizz(int $playerTurn): bool
+    {
+        return $playerTurn % 3 === 0;
+    }
+
+    private function isBuzz($playerTurn): bool
+    {
+        return $playerTurn % 5 === 0;
     }
 }
--- php/tests/ElliotJReed/Katas/FizzBuzz/GeneratorTest.php
+++ php/tests/ElliotJReed/Katas/FizzBuzz/GeneratorTest.php
@@ -15,8 +15,33 @@ class GeneratorTest extends TestCase
         $this->generator = new Generator();
     }
 
-    public function testItReturnsOneFirst(): void
+    public function testItReturnsOneAsFirstItem(): void
     {
-        $this->assertEquals('1', $this->generator->schedule());
+        $this->assertEquals('1', $this->generator->schedule()[0]);
+    }
+
+    public function testItReturnsTwoAsSecondItem(): void
+    {
+        $this->assertEquals('2', $this->generator->schedule()[1]);
+    }
+
+    public function testItReturnsFizzAsThirdItem(): void
+    {
+        $this->assertEquals('Fizz', $this->generator->schedule()[2]);
+    }
+
+    public function testItReturnsBuzzAsFifthItem(): void
+    {
+        $this->assertEquals('Buzz', $this->generator->schedule()[4]);
+    }
+
+    public function testItReturnsFizzBuzzAsFifthteenthItem(): void
+    {
+        $this->assertEquals('FizzBuzz', $this->generator->schedule()[14]);
+    }
+
+    public function testItReturnsOneHundredListItems(): void
+    {
+        $this->assertCount(100, $this->generator->schedule());
     }
 }
